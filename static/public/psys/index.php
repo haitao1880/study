9aab97207306984120bc83a725c4093ecf9ab550
haitao1880 <haitao1880@gmail.com>
2015-12-10 11:44:50 +0800

    Update index.php

--- static/public/psys/index.php
+++ static/public/psys/index.php
@@ -1,15 +1,6 @@
 <?php
 /**
-* Copyright(c) 2013
-* 日    期:2013年11月20日
-* 文 件 名:index.php
-* 创建时间:下午5:20:05
-* 字符编码:UTF-8
-* 版本信息:$Id: index.php 74 2014-07-03 08:01:16Z tony_ren $
-* 修改日期:$LastChangedDate: 2014-07-03 16:01:16 +0800 (周四, 03 七月 2014) $
-* 最后版本:$LastChangedRevision: 74 $
-* 修 改 者:$LastChangedBy: tony_ren $
-* 版本地址:$HeadURL: http://192.168.1.100:12000/svn/rockgame/trunk/code/gamewww/public/psys/index.php $
+
 * 摘    要:引导页
 */
 error_reporting(E_ALL);
